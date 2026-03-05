0730f30c61e2c6a9dfa9066e752f6110373e9da3
Prathamjot <prathamjotsingh03@gmail.com>
2026-03-06 01:52:54 +0530

    Initial commit - fitness microservices project

---
+++ activityservice/src/main/java/com/fitness/activityservice/config/MongoConfig.java
@@ -0,0 +1,26 @@
+package com.fitness.activityservice.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+
+    private static final String CONNECTION_STRING = "mongodb://localhost:27017";
+    private static final String DATABASE_NAME = "fitnessactivity";
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create(CONNECTION_STRING);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), DATABASE_NAME);
+    }
+}
